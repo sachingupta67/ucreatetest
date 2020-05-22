6df314025a5af657979edcbe8e1ec789d5175ef0
sachingupta67 <sachingupta769@outlook.com>
2020-05-23 01:50:44 +0530

    deleted component

--- src/Layout/components/CustomButton/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Button} from 'react-native';
-
-const CustomButton = ({title, onPress}) => {
-  return <Button title={title} onPress={onPress} />;
-};
-export default CustomButton;
