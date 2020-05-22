69ed4e4e9e34b52a2ca3a200fdb164b03ae6c043
sachingupta67 <sachingupta769@outlook.com>
2020-05-23 01:50:35 +0530

    Root Component

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import MasterLayout from './src/Layout/MasterLayout';
+import AddDog from './src/screens/AddDog';
+
 const App = () => {
   return (
     <MasterLayout>
-      <View style={{borderWidth: 1}}>
-        <Text>App</Text>
+      <View style={styles.container}>
+        <AddDog />
       </View>
     </MasterLayout>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default App;
