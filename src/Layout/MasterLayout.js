b6514e6c9e74eea1d6f41f2d319078d62f9bd4d5
sachingupta67 <sachingupta769@outlook.com>
2020-05-22 23:56:30 +0530

    comments added

--- src/Layout/MasterLayout.js
+++ src/Layout/MasterLayout.js
@@ -1,3 +1,11 @@
+/*
+Purpose : Using this MasterLayout f
+           or common handling like (error handling,loading,SafeAreaView)
+           can easily apply to all component
+
+Author : Sachin Gupta
+*/
+
 import React from 'react';
 import {View, SafeAreaView, StyleSheet} from 'react-native';
 const MasterLayout = props => {
